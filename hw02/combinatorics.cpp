0f12b1222986c915acacf3864611eeba72cca91d
ge47cib <ge47cib@mytum.com>
2023-10-31 17:01:41 +0100

    erster commit make funktioniert nicht aber neue Dateien eigentlich richtig

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -2,18 +2,25 @@
 
 
 uint64_t factorial(uint64_t val) {
-    // TODO
-    return 0;
+    uint64_t a{1};
+    uint64_t i = 1;
+    while(i < val)
+    {
+        i++;
+        a = a * i;
+
+    }
+    return a;
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+    uint64_t ergebnis = factorial(val) / factorial(val-val2);
+    return ergebnis;
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+    uint64_t ergebnis = factorial(val) / factorial(val2) * factorial(val-val2);
+    return ergebnis;
 }
