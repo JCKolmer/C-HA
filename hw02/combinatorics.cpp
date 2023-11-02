1b0ede5a0fe4b1a21825d3228260a7aac3e63115
ge47cib <ge47cib@mytum.com>
2023-11-02 11:56:56 +0100

    0 hinzugefügt

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -2,25 +2,36 @@
 
 
 uint64_t factorial(uint64_t val) {
+    if(val==0)
+    {
+        return 1;
+    }
     uint64_t a{1};
     uint64_t i = 1;
     while(i < val)
     {
         i++;
         a = a * i;
-
     }
     return a;
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
+    if(val2>val)
+    {
+        return 0;
+    }
     uint64_t ergebnis = factorial(val) / factorial(val-val2);
     return ergebnis;
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
+    if(val2>val)
+    {
+        return 0;
+    }
     uint64_t ergebnis = factorial(val) / factorial(val2) * factorial(val-val2);
     return ergebnis;
 }
