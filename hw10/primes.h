ccb53bf55c7588dc7f77b866cef917b188498069
ge47cib <ge47cib@mytum.com>
2024-01-16 12:35:26 +0100

    progess bis jetzt

--- hw10/primes.h
+++ hw10/primes.h
@@ -8,11 +8,57 @@
 // Your implementation should use variadic templates, to enable calling it
 // with any number of arguments, ensure that at least two arguments are provided.
 //
+
+template <typename T1>
+constexpr T1 gcd(T1 a, T1 b) 
+{ 
+    constexpr T1 result = std::min(a, b); 
+    while (result > 0) { 
+        if (a % result == 0 && b % result == 0) { 
+            break; 
+        } 
+        result--; 
+    } 
+    return result; 
+} 
+template <typename T1, typename ... T>
+constexpr T1 gcd(T1 var1, T1 var2, const T... args) {
+    if constexpr (sizeof...(args)>0)
+    {
+        return gcd(gcd(var1,var2), args);
+    }
+    return gcd(var1,var2);
+}
+
+
 // Exercise 3:
 // Implement minimum common multiple using constexpr, such that it can be evaluated at compile time.
 // As with GCD, your implementation should use variadic templates, to enable calling it
 // with arbitrary number of arguments, but at least two
 //
+template <typename T1>
+constexpr T1 mcm(T1 a, T1 b)
+{
+    if(a==0 & b==0)
+    {
+        return 0;
+    }
+    T1 c = a * b;
+    if(c < 0)
+    {
+        c = -c;
+    }
+    return c / gcd(a,b);
+
+}
+template <typename T1, typename ... T>
+constexpr T1 mcm(T1 var1, T1 var2, const T... args) {
+    if constexpr (sizeof...(args)>0)
+    {
+        return mcm(mcm(var1,var2), args);
+    }
+    return mcm(var1,var2);
+}
 // Exercise 4:
 // Implement power using template metaprogramming. The template struct must be
 // called `Power`, and be callable with `Power<base, exponent, modulus>::value`.
--- hw10/quadruple.h
+++ hw10/quadruple.h
@@ -12,8 +12,39 @@
 template <typename T, typename U, typename V, typename W> struct Quadruple {
 public:
 
+T get_first() {return first;}
+U get_second() {return second;}
+V get_third() {return third;}
+W get_fourth() {return fourth;}
 
 private:
     T first;
+    U second;
+    V third;
+    W fourth;
 };
+template <>
+struct Quadruple <int, int, int, int> {
+public: 
+
+int get_first() {return array1[0]; }
+int get_second() {return array1[1]; }
+int get_third() {return array1[2]; }
+int get_fourth() {return array1[3]; }
+
+
+private:
+
+std::array<int, 4> array1;
+};
+
+template<typename T>
+struct  Quadruple <T,T,T,T> {
+public:
+
+private:
+
+std::array<T, 4> array2;
+};
+
 
